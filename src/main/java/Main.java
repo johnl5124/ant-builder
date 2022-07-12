6dfcbaa78f394358bedabbe860d740b223140419
John Lloyd <johnl5124@gmail.com>
2022-07-12 14:09:54 +0100

    Updated dir structure and add lib dir
    Local library jars are there, updated with default package, and began work on build.xml

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main 
+{
+	public static void main(String args[]) throws InterruptedException
+	{
+		System.out.println("Basic Robot Movement Program...");
+        
+        RobotMovement t1 = new RobotMovement();
+        
+        t1.forward();
+        
+        Thread.sleep(2000);
+        
+        t1.stop();
+	}
+}
