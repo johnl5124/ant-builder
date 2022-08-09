d3abd787c77149c32bab0d68b6616f3f4130f7f2
John Lloyd <johnl5124@gmail.com>
2022-08-09 17:06:16 +0100

    Small Edit...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
 import PrototypeTesting.PrototypeTesting.*;
 
-public class Main 
-{
-	public static void main(String args[]) throws InterruptedException
-	{
+public class Main {
+	public static void main(String args[]) throws InterruptedException {
 		System.out.println("Basic Robot Movement Program...");
-        
-        TestRobot t1 = new TestRobot();
-        
-        t1.setWheelVelocities(-100, 100, 2500);
+
+		TestRobot t1 = new TestRobot();
+
+		t1.setWheelVelocities(-100, 100, 2500);
 	}
 }
